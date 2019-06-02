e03687596960738ab3aa66d3553eb1dd01322e5f
RynBelz162 <rynbelz162@gmail.com>
2019-06-02 19:47:48 -0400

    add jetpack navigation to bottom nav view

--- app/src/main/java/com/belzsoftware/futspect/MainActivity.kt
+++ app/src/main/java/com/belzsoftware/futspect/MainActivity.kt
@@ -2,44 +2,27 @@ package com.belzsoftware.futspect
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import com.belzsoftware.futspect.ui.fixtures.FixturesFragment
-import com.belzsoftware.futspect.ui.more.MoreFragment
-import com.belzsoftware.futspect.ui.tables.TableFragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_tables -> {
-                replaceFragment(TableFragment())
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_fixtures -> {
-                replaceFragment(FixturesFragment())
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_menu -> {
-                replaceFragment(MoreFragment())
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
+    private val navController: NavController by lazy {
+        findNavController(R.id.nav_host_fragment)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        replaceFragment(TableFragment())
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        navigation.setupWithNavController(navController)
+        navigation.setOnNavigationItemSelectedListener { item ->
+            onNavDestinationSelected(item, navController)
+        }
     }
 
-    private fun replaceFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.main_content, fragment)
-            .commit()
-    }
+    override fun onSupportNavigateUp(): Boolean = navController.navigateUp()
 }
